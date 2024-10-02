a2f1b24a1e0e4d2bf394dd46db789e5d3c36f103
That-Thing <sen@onee.ch>
2024-10-01 21:36:23 -0400

    Add /endpoints endpoint.

--- src/main/java/com/dish/dish/MainController.java
+++ src/main/java/com/dish/dish/MainController.java
@@ -1,13 +1,42 @@
 package com.dish.dish;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/")
 public class MainController {
 
-    @GetMapping("/")
+    @Autowired
+    private RequestMappingHandlerMapping handlerMapping;
+
+    /**
+     * @type GET
+     * @return base URL
+     */
+    @GetMapping
     public String index() {
         return "All your base are belong to us";
+        // TODO - remove entirely or add a more meaningful message
+    }
+
+    /**
+     * @type GET
+     * @return List of all endpoints
+     */
+    @GetMapping("/endpoints")
+    public List<String> listAllEndpoints() {
+        return handlerMapping.getHandlerMethods()
+                .keySet()
+                .stream()
+                .map(requestMappingInfo -> requestMappingInfo.toString())
+                .filter(endpoint -> !endpoint.contains("/error")) // Exclude /error endpoint
+                .collect(Collectors.toList());
     }
 }
