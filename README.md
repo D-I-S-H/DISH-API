36429364a1f1036a6f84c141a06e663f00f81bab
That-Thing <sen@onee.ch>
2024-10-01 21:19:46 -0400

    Added skeleton.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# dish
-Dining Information Served Here
+# D.I.S.H
+## Dining Information Served Here
+
+---
+
+Dining Information Served Here (DISH) is a service meant to provide a better menu for Michigan Tech dining halls.
+
+---
+### Running
+
+---
+
+### Testing
+
+---
---
+++ src/main/java/com/dish/dish/MainController.java
@@ -0,0 +1,13 @@
+package com.dish.dish;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "All your base are belong to us";
+    }
+}
