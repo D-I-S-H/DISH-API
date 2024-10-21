e4f0017f57c4a83cc7e2723939d1b956cd123d0d
BaronessT03 <160291678+BaronessT03@users.noreply.github.com>
2024-10-21 13:43:01 -0400

    Database schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,31 @@
-CREATE TABLE IF NOT EXISTS account (
-   uid INTEGER PRIMARY KEY AUTOINCREMENT,
-   username TEXT NOT NULL UNIQUE,
-   password TEXT NOT NULL
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE location (
+name TEXT PRIMARY KEY,
+apiuuid CHAR(24)
+);
+
+CREATE TABLE accounts (
+username TEXT NOT NULL,
+password BLOB NOT NULL,
+uid INTEGER PRIMARY KEY AUTOINCREMENT
+);
+
+CREATE TABLE time(
+mealTime TEXT PRIMARY KEY
+);
+
+CREATE TABLE menuItems (
+name TEXT,
+ingredients TEXT,
+portion TEXT,
+description TEXT,
+nutrients TEXT,
+calories INTEGER,
+time TEXT,
+location TEXT,
+Allergens TEXT,
+PRIMARY KEY (name, location),
+FOREIGN KEY (time) REFERENCES time(mealTime),
+FOREIGN KEY (location) REFERENCES location(name)
 );
