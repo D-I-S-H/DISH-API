0383e2f571684a90279db7fd2476254c79564bda
That Thing <sen@onee.ch>
2024-10-21 13:44:38 -0400

    Beautified sql schema and removed .db file from resources directory

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,31 +1,26 @@
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE location (
-name TEXT PRIMARY KEY,
-apiuuid CHAR(24)
-);
+CREATE TABLE location (name TEXT PRIMARY KEY, apiuuid CHAR(24));
 
 CREATE TABLE accounts (
-username TEXT NOT NULL,
-password BLOB NOT NULL,
-uid INTEGER PRIMARY KEY AUTOINCREMENT
+    username TEXT NOT NULL,
+    password BLOB NOT NULL,
+    uid INTEGER PRIMARY KEY AUTOINCREMENT
 );
 
-CREATE TABLE time(
-mealTime TEXT PRIMARY KEY
-);
+CREATE TABLE time(mealTime TEXT PRIMARY KEY);
 
 CREATE TABLE menuItems (
-name TEXT,
-ingredients TEXT,
-portion TEXT,
-description TEXT,
-nutrients TEXT,
-calories INTEGER,
-time TEXT,
-location TEXT,
-Allergens TEXT,
-PRIMARY KEY (name, location),
-FOREIGN KEY (time) REFERENCES time(mealTime),
-FOREIGN KEY (location) REFERENCES location(name)
+    name TEXT,
+    ingredients TEXT,
+    portion TEXT,
+    description TEXT,
+    nutrients TEXT,
+    calories INTEGER,
+    time TEXT,
+    location TEXT,
+    Allergens TEXT,
+    PRIMARY KEY (name, location),
+    FOREIGN KEY (time) REFERENCES time(mealTime),
+    FOREIGN KEY (location) REFERENCES location(name)
 );
