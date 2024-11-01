622dbab5f6c79aa038bebd7f7ed297dd89f3b973
John Umbriac <43652625+TheRealMothebad@users.noreply.github.com>
2024-11-01 13:05:58 -0400

    Update chartwells_query.py

--- chartwells_query.py
+++ chartwells_query.py
@@ -9,7 +9,10 @@ headers = {
     'User-Agent': 'Mozilla/5.0'
 }
 
-date_today = datetime.now(timezone(timedelta(hours=-4))).strftime('%Y-%m-%d');
+#database_path = "database.db"
+database_path = "Database/dish.db"
+
+date_today = datetime.now(timezone(timedelta(hours=-4))).strftime('%Y-%m-%d')
 date_tomorrow = (datetime.now(timezone(timedelta(hours=-4))) + timedelta(1)).strftime('%Y-%m-%d')
 dates = {"today":date_today}#,"tomorrow":date_tomorrow}
 
@@ -22,7 +25,7 @@ meal_data_request = "https://api.dineoncampus.com/v1/location/{location}/periods
 dining_locations = {"Wadsworth":"64b9990ec625af0685fb939d"}#,"McNair":"64a6b628351d5305dde2bc08","DHH":"64e3da15e45d430b80c9b981"}
 
 def main():
-    db_conection = sqlite3.connect("database.db")
+    db_conection = sqlite3.connect(database_path)
     db_cursor = db_conection.cursor()
 
     for date in dates:
@@ -31,25 +34,40 @@ def main():
             periods = get_periods(date, location)
 
             print(periods)
-            #TODO: update periods with current values, will be tricky
-            #db_cursor.executemany("INSERT INTO time VALUES (:name, :)")
+
+            #db_cursor.executemany("DELETE FROM time WHERE mealTime=:name", periods)
+            db_cursor.executemany("INSERT OR REPLACE INTO time VALUES (:name, :UUID)", periods)
 
             for period in periods:
-                meal_json = get_meal_data(periods[period], date, location)
+                meal_json = get_meal_data(period["UUID"], date, location)
                 if meal_json != -1:
                     for station in meal_json["menu"]["periods"]["categories"]:
-                        print(station)    
                         station["location"] = location
                     db_cursor.executemany("INSERT OR REPLACE INTO stations VALUES (:name, :location)", meal_json["menu"]["periods"]["categories"])
 
                     for station in meal_json["menu"]["periods"]["categories"]:
-                        for meal in station["items"]:
-                            meal["time"] = period
-                            meal["location"] = location
-                            meal["station"] = station["name"]
-                            meal["nutrients_json"] = str(meal["nutrients"])
-                            meal["filters_json"] = str(meal["filters"])
-                        db_cursor.executemany("INSERT INTO menuItems VALUES (:name, :station, :ingredients, :portion, :desc, :nutrients_json, :calories, :time, :location, :filters_json)", station["items"])
+                        for item in station["items"]:
+                            item["time"] = period["name"]
+                            item["location"] = location
+                            item["station"] = station["name"]
+                            item["nutrients_json"] = handle_nutrients(db_cursor, item)
+                            
+                            allergens_list = []
+                            filters_list = []
+                            for filter in item["filters"]:
+                                if filter["type"] == "allergen":
+                                    allergens_list.append(filter["name"])
+                                else:
+                                    filters_list.append(filter["name"])
+                            item["allergens_json"] = str(allergens_list)
+                            item["filters_json"] = str(filters_list)
+
+                        db_cursor.executemany("INSERT OR REPLACE INTO menuItems VALUES (:name, :station, :ingredients, :portion, :desc, :nutrients_json, :calories, :time, :location, :allergens_json, :filters_json)", station["items"])
+
+                        for item in station["items"]:
+                            db_cursor.executemany("INSERT OR REPLACE INTO menuFilters VALUES (:name, :type)", item["filters"])
+                            for filter in item["filters"]:
+                                db_cursor.execute("INSERT OR REPLACE INTO itemFilterAssociations VALUES (?, ?, ?, ?, ?)", [item["name"], item["location"], item["time"], item["station"], filter["name"]])
 
 
                     db_conection.commit()
@@ -64,15 +82,18 @@ def get_periods(date, location):
     if response.status_code != 204:
         response_json = response.json()
 
-        periods = {}
+        period_list = []
         if not response_json["closed"]:
             for period in response_json["periods"]:
-                periods[period["name"]] = period["id"]
-        return periods
+                periods = {}
+                periods["name"] = period["name"]
+                periods["UUID"] = period["id"]
+                period_list.append(periods)
+        return period_list
     else:
         #TODO: Throw a nasty error here or something
         print(f"ERROR: status code 204 not returned for {date} {location}")
-        return {}
+        return []
 
 def get_meal_data(period, date, location):
     request_string =  meal_data_request.format(period = period, date = dates[date], location = dining_locations[location])
@@ -83,13 +104,29 @@ def get_meal_data(period, date, location):
     if response.status_code != 204:
         response_json = response.json()
 
-        return response_json;
+        return response_json
     else:
         #TODO: Throw a nasty error here or something
         print(f"ERROR: status code 204 not returned for {period} {date} {location}")
         return -1
 
-
+def handle_nutrients(db_cursor, item):
+    nutrients_list = []
+    nutrients_association_list = []
+    for nutrient in item["nutrients"]:
+        dictionary = {}
+        dictionary["name"] = nutrient["name"]
+        dictionary["value"] = nutrient["value"]
+        dictionary["uom"] = nutrient["uom"]
+        nutrients_list.append(dictionary.copy())
+        dictionary["itemName"] = item["name"]
+        dictionary["itemLocation"] = item["location"]
+        dictionary["itemTime"] = item["time"]
+        dictionary["itemStation"] = item["station"]
+        nutrients_association_list.append(dictionary.copy())
+    db_cursor.executemany("INSERT OR REPLACE INTO menuNutrients VALUES (:name, :value, :uom, :value_numeric)", item["nutrients"])
+    db_cursor.executemany("INSERT OR REPLACE INTO itemNutrientAssociations VALUES (:itemName, :itemLocation, :itemTime, :itemStation, :name, :value)", nutrients_association_list)
+    return str(nutrients_list)
 
 if __name__=="__main__":
     main()
