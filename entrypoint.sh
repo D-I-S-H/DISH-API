536a9bff6a1dc21b96effb84821e1913785c1c56
That Thing <sen@onee.ch>
2024-11-02 12:35:51 -0400

    Trying to fix for azure.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-# Run at startup
+
+java -jar /app/app.jar &
+
+# Wait until server starts
+echo "Waiting for the Spring Boot application to start..."
+until nc -z localhost 8080; do
+  sleep 5
+done
+echo "Started"
+
+# Run python script
 python3 /app/chartwells_query.py
 
-# Start dcron
+# Start cron in the background
 crond -b
 
-touch ./debug.file
-
-# Start DISH
-java -jar /app/app.jar
+wait -n
